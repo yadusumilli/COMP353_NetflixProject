6c4abc0723be4874066db71d4b97cd3d46f371be
jakobveselsky <jveselsky@luc.edu>
2021-05-04 01:00:27 -0500

    addition of small bug fixes

--- Netflix-App-Final/Netflix-App-Final/flaskDemo/routes.py
+++ Netflix-App-Final/Netflix-App-Final/flaskDemo/routes.py
@@ -108,7 +108,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(User.password, form.password.data):
-            login_user(sser, remember=form.remember.data)
+            login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('Movies_TVShows'))
         else:
