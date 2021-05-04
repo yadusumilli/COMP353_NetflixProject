cc1656fe8e6cdb8519952ff7537288066cbd74b8
jakobveselsky <jveselsky@luc.edu>
2021-05-04 13:03:46 -0500

    addition of login functionalty

--- Netflix-App-Final/Netflix-App-Final/flaskDemo/routes.py
+++ Netflix-App-Final/Netflix-App-Final/flaskDemo/routes.py
@@ -107,7 +107,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(User.password, form.password.data):
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('Movies_TVShows'))
